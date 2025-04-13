0a5683be46dc1b8d0acaf82b86491823d8afd2a0
Leonardo Efigenio <leonardo.efigenio.22@gmail.com>
2025-04-13 11:56:12 -0600

    Maquetación de selección de componentes

--- Assets/Scripts/SelectTypeManager.cs
+++ Assets/Scripts/SelectTypeManager.cs
@@ -5,15 +5,11 @@ public class SelectTypeManager : MonoBehaviour
 {
     public void SelectLaptop()
     {
-        Debug.Log("Se eligió Laptop");
-        // Aquí cargarás la escena del ensamblaje de laptop
         SceneManager.LoadScene("AssemblyLaptop");
     }
 
     public void SelectDesktop()
     {
-        Debug.Log("Se eligió Desktop");
-        // Aquí cargarás la escena del ensamblaje de desktop
         SceneManager.LoadScene("AssemblyDesktop");
     }
 }
